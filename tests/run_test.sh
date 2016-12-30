0555273c056f885e4f086129089c7c57fa6a0a0b
张杰 <zhangjiehz@chinamobile.com>
2016-12-30 17:26:37 +0800

    单元测试脚本

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,5 +2,5 @@
 nosetests -sv tests/test_final.py
 nosetests -sv tests/test_token.py
 nosetests -sv tests/test_kcli/test_ssh_cli.py
-nosetests -s tests/test_confloader/test_confloader.py
+#nosetests -s tests/test_confloader/test_confloader.py
 
